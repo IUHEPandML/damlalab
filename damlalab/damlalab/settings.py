ac29cc64ff3ed34c7802fe99673ae9d440363f5d
Burak Talha <alptng72@gmail.com>
2026-03-13 01:12:41 +0300

    Fix Allowed Hosts for Render

--- damlalab/damlalab/settings.py
+++ damlalab/damlalab/settings.py
@@ -21,6 +21,9 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# settings.py
+
+ALLOWED_HOSTS = ['damlalab.onrender.com', 'localhost', '127.0.0.1']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
